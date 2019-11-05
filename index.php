a0e722f51277e33bcd58d68ed3c17ef88edfe105
Biny <1559867170@qq.com>
2019-11-05 18:35:50 +0800

    0.3

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
+<!--加载全局配置文件-->
+<?php include 'controller/config.php'; ?>
 <!--加载公共头部-->
-<?php  include 'public/inc/header_inc.php'; ?>
-
+<?php include 'view/inc/header_inc.php'; ?>
 <!--加载当前页面模板-->
-<?php  auto_load_tpl($tplName);  ?>
-
-<!-- 加载公共底部-->
-<?php include 'public/inc/footer_inc.php' ?>
-
+<?php auto_load_tpl($tplName);  ?>
+<!--加载公共底部-->
+<?php include 'view/inc/footer_inc.php' ?>
--- version 0.1/index.php
+++
@@ -1,8 +0,0 @@
-<!--加载全局配置文件-->
-<?php include 'controller/config.php'; ?>
-<!--加载公共头部-->
-<?php include 'view/inc/header_inc.php'; ?>
-<!--加载当前页面模板-->
-<?php auto_load_tpl($tplName);  ?>
-<!--加载公共底部-->
-<?php include 'view/inc/footer_inc.php' ?>
