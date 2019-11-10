59f8901803ccbfc55ab556a9a09ee44671e8136a
Biny <1559867170@qq.com>
2019-11-10 15:17:28 +0800

    v2

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * 我们项目中用到的配置信息,在这里需要配置我们对应数据库的信息，如数据库名字，密码和用户名，需要跟自己本地的对应
+ */
+
+/**
+ * 数据库主机
+ */
+define('DB_HOST', 'mysql.coding.io');
+/**
+ * 数据库用户名
+ */
+define('DB_USER', 'user-nnfpQ9KK2z');
+/**
+ * 数据库密码
+ */
+define('DB_PASS', 'n_QQ9c]SKXgeqcOs{wks');
+/**
+ * 数据库名字
+ */
+define('DB_NAME', 'db-eYPQ9bNIXb');
