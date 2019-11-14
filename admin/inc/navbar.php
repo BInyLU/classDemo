75415724c6d15426e4a70a7d1e8235b09de3b8dc
Biny <1559867170@qq.com>
2019-11-14 14:40:47 +0800

    5.1

--- admin/inc/navbar.php
+++ admin/inc/navbar.php
@@ -1,6 +1,8 @@
  <nav class="navbar">
       <button class="btn btn-default navbar-btn fa fa-bars"></button>
       <ul class="nav navbar-nav navbar-right">
+        <li><a href="/admin/profile.php"><i class="fa fa-home"></i>首页</a></li>
+        <li><a href="/admin/profile.php"><i class="fa fa-book"></i>话题研讨</a></li>
         <li><a href="/admin/profile.php"><i class="fa fa-user"></i>个人中心</a></li>
         <li><a href="/admin/login.php?action=logout"><i class="fa fa-sign-out"></i>退出</a></li>
       </ul>
